e4d872bff2d3cbac49c816fc692ce83bdf83372b
715484127 <715484127@qq.com>
2018-01-18 00:37:00 +0800

    first

--- angular2-reddit/src/app/article/article.component.ts
+++ angular2-reddit/src/app/article/article.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css'],
+  // tslint:disable-next-line:use-host-property-decorator
   host: {class: 'row'}
 })
 export class ArticleComponent implements OnInit {
-  article: Article;
+  @Input() article: Article;
 
   constructor() {
     this.article = new Article(
