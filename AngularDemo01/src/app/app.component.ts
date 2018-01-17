c51eadb063d57d6ed93dc6eee7064f614a29b24e
715484127 <715484127@qq.com>
2018-01-17 18:00:35 +0800

    first

--- AngularDemo01/src/app/app.component.ts
+++ AngularDemo01/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Login } from './app.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  login: Login;
+
+  dologin(userName: HTMLInputElement, password: HTMLInputElement) {
+    console.log(userName.value + '=====' + password.value);
+  }
 }
